a24675cbc182526c1b22469a2b9932d21a61016b
root <root@localhost.localdomain>
2021-04-09 14:59:40 +0200

    first commit

---
+++ controlLogix_client.py
@@ -0,0 +1,56 @@
+# Roy Teneij
+from pycomm3 import LogixDriver, CIPDriver, Services
+
+def getPlcInfo():
+	with LogixDriver('10.0.111.5') as plc:
+		return(plc)
+
+def getTags():
+	with LogixDriver('10.0.111.5') as plc:
+		return plc.tags
+
+def readTag(tagName):
+	with LogixDriver('10.0.111.5') as plc:
+		return plc.read(tagName)
+
+def writeTag():
+	with LogixDriver('10.0.111.5') as plc:
+		data = {'tag_name': 'System_In_Simulation', 'dim': 0, 'instance_id': 448, 'symbol_address': 1879793412, 'symbol_object_address': 1947314424, 'software_control': 1140916492, 'alias': False, 'external_access': 'Read/Write', 'dimensions': [0, 0, 0], 'tag_type': 'atomic', 'data_type': 'BOOL', 'data_type_name': 'BOOL', 'bit_position': 0}
+		return plc.write(('System_In_Simulation', data))
+
+def getIPConfig():
+    message_path = '10.0.111.5'
+
+    with CIPDriver(message_path) as plc:  # L85
+        data = plc.generic_message(
+            service=b'\x0e',
+            class_code=b'\xf5',
+            instance=1,
+            attribute=3,
+            connected=False,
+            unconnected_send=True,
+            route_path=True,
+            name='IP_config'
+        )
+
+        statuses = {
+            0b_0000: 'static',
+            0b_0001: 'BOOTP',
+            0b_0010: 'DHCP'
+        }
+        print(data.value)
+       #ip_status = data.value & 0b_1111  # only need the first 4 bits
+       # print(statuses.get(ip_status, 'unknown'))
+
+def stopPlc():
+	with CIPDriver('10.0.111.5') as plc:
+		data = plc.generic_message(
+			service=b'\x07',
+			class_code=b'\x24',
+			instance=1,
+			connected=False,
+			unconnected_send=True,
+			route_path=True,
+			name='STOP_plc' 
+		)
+		print(data.value)
